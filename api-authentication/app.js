2bbc8bafb2e31ab1f77cc9c9be9f4a0177ba86cd
shalitha <shalithax@gmail.com>
2022-05-14 21:53:58 +0530

    configure backend

---
+++ api-authentication/app.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.get("/auth", (req, res) => {
+    res.send("Welcome to ProjectService")
+})
+app.listen(4500, () => {
+    console.log("Listening on PORT 4500")
+})
